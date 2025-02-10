61bbcd8277d648eee89d5987e5e3fd94b539825e
Hendrix Gullixson <hgullixson@gmail.com>
2025-02-10 14:40:41 -0600

    added chat

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,39 @@ function App() {
 
 
 
-        <div 
+        <div
           className="card w-full bg-base-100 card-lg shadow-sm mt-4"
         >
           <div className="card-body">
             <h2 className="card-title">Leaderboard</h2>
-            <p>Leaderboard Coming Soon...</p>
+
+
+            <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-primary">What kind of nonsense is this</div>
+            </div>
+            <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-secondary">Put me on the Council and not make me a Master!??</div>
+            </div>
+            <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-accent">That's never been done in the history of the Jedi.</div>
+            </div>
+            <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-neutral">It's insulting!</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-info">Calm down, Anakin.</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-success">You have been given a great honor.</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-warning">To be on the Council at your age.</div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-error">It's never happened before.</div>
+            </div>
+
+
             <div className="justify-end card-actions">
               <button className="btn btn-primary">Buy Now</button>
             </div>
