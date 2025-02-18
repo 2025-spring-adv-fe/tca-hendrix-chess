5cbbdd23bbfa10235043720efbc32727d266881a
Hendrix Gullixson <hgullixson@gmail.com>
2025-02-18 13:40:39 -0600

    Created Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,9 @@ import {
   , Routes
   , Route
 } from 'react-router';
-
-const Home = () => {
-
-return (
-  <>
-  <h3
-      className='text-2xl font-bold'
-      >
-      Home
-      </h3>
-      <button
-      className='btn btn-active btn-secondary btn-large'
-      >
-        Play Chess
-      </button>
-  
-  </>
-)
+import { Home } from './Home';
 
 
-}
 
 
 const App = () => {
---
+++ src/Home.tsx
@@ -0,0 +1,20 @@
+export const Home = () => {
+
+    return (
+      <>
+      <h3
+          className='text-2xl font-bold'
+          >
+          Home
+          </h3>
+          <button
+          className='btn btn-active btn-secondary btn-large'
+          >
+            Play Chess
+          </button>
+      
+      </>
+    )
+    
+    
+    }
