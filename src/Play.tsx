d7df07bb92d9a6ef73a557f9f86cfff1623c14ff
Hendrix Gullixson <hgullixson@gmail.com>
2025-02-25 13:09:28 -0600

    updated turn number

--- src/Play.tsx
+++ src/Play.tsx
@@ -1,16 +1,34 @@
+import { useState } from "react";
 import { useNavigate } from "react-router"
 
 export const Play = () => {
 
   const navThree = useNavigate();
+  const [turnNumber, setTurnNumber] = useState(6);
 
     return (
       <>
       <h3
           className='text-2xl font-bold'
           >
-          Play
-          </h3>
+            Play
+            
+      </h3>
+            <h4>
+            Turn #{turnNumber}
+            <button
+              className="btn btn-xs btn-outline btn-light ml-4"
+              onClick={
+                () => {
+                  setTurnNumber(turnNumber + 1);
+                    console.log(turnNumber);
+                  
+                }
+              }
+              >
+                +
+              </button>
+              </h4>
           <button
           className='btn btn-active btn-secondary btn-large mt-4'
           onClick={
