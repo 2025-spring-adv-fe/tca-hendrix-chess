41afa7456193996c6543f0a12342f867e02e13f0
Hendrix Gullixson <hgullixson@gmail.com>
2025-02-27 17:02:35 -0600

    Card placeholder for leaderboard...

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,8 +2,8 @@ import { useNavigate } from "react-router"
 import { LeaderboardEntry } from "./GameResults";
 
 interface HomeProps {
-    totalGameCount: number;
-    leaderboardData: LeaderboardEntry[];
+  totalGameCount: number;
+  leaderboardData: LeaderboardEntry[];
 };
 
 
@@ -15,24 +15,31 @@ export const Home: React.FC<HomeProps> = ({
   console.log(leaderboardData);
 
   const nav = useNavigate();
-    return (
-      <>
+  return (
+    <>
       <h3
-          className='text-2xl font-bold'
-          >
-          Home ({totalGameCount}0 games played)
-          </h3> 
-          <button
-          className='btn btn-active btn-secondary btn-large'
-          onClick={
-            () => nav("/setup")
-          }
-          >
-            Play Chess
-          </button>
-      
-      </>
-    )
-    
-    
-    }
+        className='text-2xl font-bold'
+      >
+        Home ({totalGameCount}0 games played)
+      </h3>
+      <button
+        className='btn btn-active btn-secondary btn-large'
+        onClick={
+          () => nav("/setup")
+        }
+      >
+        Play Chess
+      </button>
+
+      <div className="card w-96 bg-base-100 card-md shadow-lg">
+        <div className="card-body">
+          <h2 className="card-title">Leaderboard</h2>
+          <p>Leaderboard goes here!</p>
+        </div>
+      </div>
+
+    </>
+  )
+
+
+}
